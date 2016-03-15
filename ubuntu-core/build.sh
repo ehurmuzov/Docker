b62dd638ca417a642815d4b01da8bab934d2dd0c
realsoft <emil@hurmuzov.com>
2016-03-15 15:32:09 +0200

    ubuntu-core; java

---
+++ ubuntu-core/build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+set -x
+
+VER=14.04.3
+VERSHORT=$(echo $VER | sed 's/\(^[0-9][0-9]*\.[0-9][0-9]*\).*/\1/g')
+FILE=ubuntu-core-${VER}-core-amd64.tar.gz
+URL=http://cdimage.ubuntu.com/ubuntu-core/releases/${VERSHORT}/release/${FILE}
+SHA256SUMURL=http://cdimage.ubuntu.com/ubuntu-core/releases/${VERSHORT}/release/SHA256SUMS
+
+check()
+{
+  grep $FILE SHA256SUMS | sha256sum -c
+}
+
+if [ -f SHA256SUMS ]
+then
+  rm -f SHA256SUMS
+fi
+
+wget $SHA256SUMURL
+
+if [ -e $FILE ]
+then
+  if ! check
+  then
+    rm -f $FILE
+  fi
+fi
+
+if [ ! -e $FILE ]
+then
+  wget $URL
+  check
+fi
+
+#cat $FILE | docker import - realsoft/ubuntu-core:${VER}
+docker import $FILE realsoft/ubuntu-core:${VER}
